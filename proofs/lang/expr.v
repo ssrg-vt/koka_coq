10bedf2a6c6d099972a3f9bacaecd17c7303541d
Swarn Priya <priya13swarn@gmail.com>
2024-09-18 12:15:39 -0400

    .

--- proofs/lang/expr.v
+++ proofs/lang/expr.v
@@ -4,7 +4,7 @@ Require Import Coq.FSets.FSetProperties Coq.FSets.FMapFacts FMaps FSetAVL Nat Pe
 Require Import Coq.Arith.EqNat Coq.ZArith.Int Integers AST types Maps.
 
 Inductive constant : Type :=
-| ConsInt : int -> constant
+| ConsInt : Z -> constant
 | ConsBool : bool -> constant
 | ConsUnit : constant.
 
@@ -15,17 +15,24 @@ Inductive expr : Type :=
 | Const : constant -> expr                                (* constant *)
 | Abs : nat -> list (ident * type) -> expr -> expr        (* f(x1,.., xn) = e *)
 | App : expr -> nat -> list expr -> expr                  (* e en *)
-| Addr : ident -> expr                                    (* address *)
-| Ref : type -> expr -> expr                              (* allocation : ref t e allocates e of type t and returns the fresh address *)
-| Deref : type -> expr -> expr                            (* dereference : deref t e returns the value of type t present at location e *)
-| Run : expr -> expr                                      (* eliminate heap effect : [r1-> v1, ..., ern->vn] e reduces to e  
-                                                             captures the essence of state isolation and reduces to a value discarding the heap *)
+| Addr : type -> ident -> expr                            (* address *)
+| Ref : type -> expr -> expr                              (* allocation : ref t e allocates e of type t 
+                                                             and returns the fresh address *)
+| Deref : type -> expr -> expr                            (* dereference : deref t e returns the value of type t 
+                                                             present at location e *)
+| Run : expr -> expr                                      (* eliminate heap effect : [r1-> v1, ..., ern->vn] e 
+                                                             reduces to e captures the essence of state isolation 
+                                                             and reduces to a value discarding the heap *)
 | Sexpr : expr -> expr                                    (* assign value at a location l (l := e) 
                                                              assigns the evaluation of e to the reference cell l *)
-| Hexpr : ident -> expr -> expr                           (* heap effect *) (* ident should be heapmap *) 
+| Hexpr : heap -> expr -> expr                            (* heap effect *) 
 | Lexpr : ident -> type -> expr -> expr                   (* let binding *)
-| Cond : expr -> expr -> expr                             (* if e then e else e *).
+| Cond : expr -> expr -> expr                             (* if e then e else e *)
+with heap : Type := 
+| H : list (ident * expr) -> heap.
 
+Scheme expr_heap_rec := Induction for expr Sort Set
+ with heap_expr_rec := Induction for heap Sort Set.
 
 (* A value is either:
 - a constant
@@ -39,22 +46,45 @@ Inductive value: expr -> Prop :=
            value (Const c)
 | Vabs : forall n xt e,
          value (Abs n xt e)
-| Vloc : forall l,
-         value (Addr l).
+| Vloc : forall t l,
+         value (Addr t l).
 
-Definition heap := list (ident * expr).
 Definition vmap := list (ident * expr).
-         
+
+(* Typing Rules *)
+Inductive ty_expr : ty_context -> expr -> type -> effect -> Type :=
+(* Since the variable and constant evaluation produces no effect, 
+   we are free to assume any arbitrary effect *)
+| Ty_var : forall Gamma t x ef,
+           get_ty Gamma x = Some t ->
+           ty_expr Gamma (Var t x) t ef
+| Ty_const_int : forall Gamma i ef,
+                 ty_expr Gamma (Const (ConsInt i)) (Btype (Bprim Tint)) ef
+| Ty_const_bool : forall Gamma b ef,
+                  ty_expr Gamma (Const (ConsBool b)) (Btype (Bprim Tbool)) ef
+| Ty_const_unit : forall Gamma ef,
+                  ty_expr Gamma (Const (ConsUnit)) (Btype (Bprim Tunit)) ef
+| Ty_abs : forall Gamma Gamma' n xs e t2 ef2 ef,
+           extend_contexts Gamma xs = Gamma' ->
+           ty_expr Gamma' e t2 ef2 ->
+           ty_expr Gamma (Abs n xs e) (Ftype (unzip2 xs) n ef2 t2) ef
+| Ty_app : forall Gamma e es ts n ef t1,
+           ty_expr Gamma e (Ftype ts n ef t1) ef ->
+           ty_exprs Gamma es ts ef ->
+           ty_expr Gamma (App e n es) t1 ef
+(*| Ty_addr : forall Gamma t l,
+            ty_expr Gamma (Addr t l) (Reftype *)
+with ty_exprs : ty_context -> list expr -> list type -> effect -> Prop :=
+| Ty_nil : forall Gamma,
+           ty_exprs Gamma nil [::(Btype(Bprim Tunit))] Empty
+| Ty_cons : forall Gamma e es t ef ts efs,
+            ty_expr Gamma e t ef ->
+            ty_exprs Gamma es ts efs ->
+            ty_exprs Gamma (e :: es) (t :: ts) (Erow ef efs).
 
 
-(*(* Memory and Variable Map *)
-Module M := FMapAVL.Make  Positive_as_OT.
-Module P := WProperties_fun Positive_as_OT M.
-Module F := P.F.
-
-(* Map from ident to val: Map is build using a AVL tree for fast operations *)
-Definition heap := M.t val. 
-Definition vmap := M.t val.*)
+Scheme ty_expr_exprs_rec := Induction for ty_expr Sort Prop
+ with ty_exprs_expr_rec := Induction for ty_exprs Sort Prop.
 
 
 
--- proofs/lang/types.v
+++ proofs/lang/types.v
@@ -10,31 +10,133 @@ Inductive effect_atom : Type :=
 
 Inductive effect : Type :=
 | Empty : effect                           (* empty effect *)
-| ESeq : effect_atom -> effect -> effect   (* row of effects *)
+| Esingle : effect_atom -> effect          (* single effect *)
+| Erow : effect -> effect -> effect        (* row of effects *)
 | Evar : ident -> effect                   (* effect variable *).
-
-Inductive signedness : Type :=
-| Signed: signedness
-| Unsigned: signedness.
-
-Inductive intsize : Type :=
-| I8: intsize
-| I16: intsize
-| I32: intsize
-| IBool: intsize.
 
 Inductive primitive_type : Type :=
 | Tunit : primitive_type
-| Tint : intsize -> signedness -> primitive_type
-| Tbool : bool -> primitive_type.
+| Tint : primitive_type
+| Tbool : primitive_type.
 
 Inductive basic_type : Type :=
 | Bprim : primitive_type -> basic_type 
 | Bpair : nat -> list basic_type -> basic_type    (* pair of types *).
 
 Inductive type : Type :=
-| Btype : basic_type -> type                      (* basic types *)
-| Ftype : list type -> nat -> effect -> type      (* function/arrow type *)
-| Reftype : effect -> basic_type -> type          (* reference type ref<h,int> *)
-| Ptype : nat -> list type -> type                (* pair type *). 
+| Btype : basic_type -> type                              (* basic types *)
+| Ftype : list type -> nat -> effect -> type -> type      (* function/arrow type *)
+| Reftype : effect -> basic_type -> type                  (* reference type ref<h,int> *)
+| Ptype : nat -> list type -> type                        (* pair type *). 
+
+
+(* Equality on types *)
+Definition eq_effect_atom (e1 e2 : effect_atom) : bool :=
+match e1, e2 with 
+| Exn, Exn => true 
+| Div, Div => true 
+| Hst id1, Hst id2 => (id1 =? id2)%positive
+| _, _ => false
+end.
+
+Fixpoint eq_effect (e1 e2 : effect) : bool :=
+match e1, e2 with 
+| Empty, Empty => true 
+| Esingle e, Esingle e' => eq_effect_atom e e'
+| Erow e es, Erow e' es' => eq_effect e e' && eq_effect es es'
+| Evar id1, Evar id2 => (id1 =? id2)%positive
+| _, _ => false
+end.
+
+Definition eq_primitive_type (p1 p2 : primitive_type) : bool :=
+match p1, p2 with 
+| Tunit, Tunit => true 
+| Tint, Tint => true
+| Tbool, Tbool => true
+| _, _ => false
+end.   
+
+Section Eq_basic_types.
+
+Variable eq_basic_type : basic_type -> basic_type -> bool.
+
+Fixpoint eq_basic_types (bs1 bs2 : list basic_type) : bool :=
+match bs1, bs2 with 
+| nil, nil => true 
+| x :: xs, x' :: xs' => eq_basic_type x x' && eq_basic_types xs xs'
+| _, _ => false
+end.
+
+End Eq_basic_types.
+
+Fixpoint eq_basic_type (b1 b2 : basic_type) : bool :=
+match b1, b2 with 
+| Bprim p1, Bprim p2 => eq_primitive_type p1 p2
+| Bpair n1 es1, Bpair n2 es2 => (n1 =? n2) && eq_basic_types eq_basic_type es1 es2
+| _, _ => false
+end.
+
+Section Eq_types.
+
+Variable eq_type : type -> type -> bool.
+
+Fixpoint eq_types (t1 t2: list type) : bool :=
+match t1, t2 with 
+| nil, nil => true 
+| x :: xs, x' :: xs' => eq_type x x' && eq_types xs xs'
+| _, _ => false
+end.
+
+End Eq_types.
+
+Fixpoint eq_type (t1 t2 : type) : bool :=
+match t1,t2 with 
+| Btype b1, Btype b2 => eq_basic_type b1 b2
+| Ftype ts1 n1 e1 t1, Ftype ts2 n2 e2 t2 => 
+  eq_types eq_type ts1 ts2 && (n1 =? n2)%nat && eq_effect e1 e2 && eq_type t1 t2
+| Reftype e1 b1, Reftype e2 b2 => eq_effect e1 e2 && eq_basic_type b1 b2 
+| Ptype n1 ts1, Ptype n2 ts2 => (n1 =? n2)%nat && eq_types eq_type ts1 ts2
+| _, _ => false
+end.
+
+
+(* Typing context *)
+Definition ty_context := list (ident * type).
+Definition store_context := list (ident * type).
+
+Fixpoint remove_var_ty (t : ty_context) (k : ident) (T : type) : ty_context :=
+match t with 
+| nil => nil 
+| x :: xs => if ((k =? x.1)%positive && (eq_type T x.2)) then xs else x :: remove_var_ty xs k T
+end.
+
+Fixpoint is_mem (k : ident) (t : ty_context) : bool :=
+match t with 
+| nil => false
+| x :: xs => if (k =? x.1)%positive then true else is_mem k xs
+end.
+
+Fixpoint extend_context (t : ty_context) (k : ident) (v : type) : ty_context := 
+match t with 
+| nil => [:: (k, v)]
+| h :: t => if (k =? h.1)%positive then (k, v) :: t else h :: extend_context t k v
+end. 
+
+Fixpoint append_context (t1 : ty_context) (t2 : ty_context) : ty_context :=
+match t2 with 
+| nil => t1
+| h :: t =>  append_context (extend_context t1 h.1 h.2) t
+end.
+
+Fixpoint get_ty (t : ty_context) (k : ident) : option type :=
+match t with 
+| nil => None 
+| x :: xs => if (x.1 =? k)%positive then Some x.2 else get_ty xs k
+end.
+
+Fixpoint extend_contexts (t : ty_context) (ks : list (ident * type)) : ty_context := 
+match ks with 
+| nil => t
+| k :: ks => extend_contexts (extend_context t k.1 k.2) ks
+end. 
 
